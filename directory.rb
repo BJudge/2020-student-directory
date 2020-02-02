017696a27abdc60b5e3785e497ca88c342d2419e
BartJudge <BartJudge@gmail.com>
2020-02-02 14:19:37 +0000

    addiming user input

--- directory.rb
+++ directory.rb
@@ -1,16 +1,30 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :November},
-  {name: "Darth Vader", cohort: :November},
-  {name:"Nurse Ratched", cohort: :November},
-  {name: "Michael Corleone", cohort: :November},
-  {name: "Alex DeLarge", cohort: :November},
-  {name: "The Wicked Witch of the West", cohort: :November},
-  {name: "Terminator", cohort: :November},
-  {name: "Freddy Krueger", cohort: :November},
-  {name: "The Joker", cohort: :November},
- {name: "Joffrey Baratheon", cohort: :November},
- {name: "Norman Bates", cohort: :November},
-]
+#students = [
+#  {name: "Dr. Hannibal Lecter", cohort: :November},
+#  {name: "Darth Vader", cohort: :November},
+#  {name:"Nurse Ratched", cohort: :November},
+#  {name: "Michael Corleone", cohort: :November},
+#  {name: "The Wicked Witch of the West", cohort: :November},
+#  {name: "Terminator", cohort: :November},
+#  {name: "Freddy Krueger", cohort: :November},
+#  {name: "The Joker", cohort: :November},
+ #{name: "Joffrey Baratheon", cohort: :November},
+ #{name: "Norman Bates", cohort: :November},
+#]
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :November}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
 
 def print_header
   puts "The students of Villains Academy"
@@ -25,6 +39,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
