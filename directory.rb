22e35e5fb544efa43d322e21f6c978b653ebcb10
BartJudge <BartJudge@gmail.com>
2020-02-03 12:41:27 +0000

    adding interactive menu

--- directory.rb
+++ directory.rb
@@ -11,6 +11,38 @@
 #{name: "Norman Bates", cohort: :November},
 #]
 
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Search for student by first letter"
+    puts "4. Search for student by length of name"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "3"
+      puts "Search for a student by first letter".center(50)
+      letter = gets.chomp
+      search_by_letter(students, letter)
+    when "4"
+      puts "Search for a student by length of their name".center(50)
+      length_input = gets.delete!("\n").to_i
+      search_by_length(students, length_input)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -56,13 +88,4 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students".center(50)
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
-puts "Search for a student by first letter".center(50)
-letter = gets.chomp
-search_by_letter(students, letter)
-puts "Search for a student by length of their name".center(50)
-length_input = gets.delete!("\n").to_i
-search_by_length(students, length_input)
+interactive_menu
